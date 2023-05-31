96d8fe7544d480d21469fad498e2ff0252bb868d
Ezequiel Agustín Ochoa <agustinmirotti97@gmail.com>
2023-05-31 15:45:36 -0300

    Primer commit

---
+++ primer-parcial/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
